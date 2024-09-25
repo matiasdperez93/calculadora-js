b4eab4c53497a22d2a474b17391e655d38e360b6
Matias Perez <matperez@frba.utn.edu.ar>
2024-09-25 15:38:20 -0300

    cree una calculadora basica

---
+++ script.js
@@ -0,0 +1,14 @@
+//agrega a la pantalla el valor del boton de la calculadora
+function agregar(valor){
+    document.getElementById('pantalla').value += valor
+}
+
+function borrar(){
+    document.getElementById('pantalla').value =''
+}
+
+function calcular(){
+    const valorPantalla = document.getElementById('pantalla').value
+    const resultado = eval(valorPantalla)
+    document.getElementById('pantalla').value = resultado
+}
